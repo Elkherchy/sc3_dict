61a805063f4fa0d6e397c1e99b72bc6d667b9f95
Elkherchy <elkherchymd22025@gmail.com>
2025-03-16 21:38:35 +0000

    update

--- core/views.py
+++ core/views.py
@@ -348,7 +348,7 @@ class FileUploadView(APIView):
     def post(self, request, *args, **kwargs):
         file_serializer = UploadedDocumentSerializer(data=request.data)
         if file_serializer.is_valid():
-            file_serializer.save(uploaded_by=request.uploaded_by)
+            file_serializer.save()
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
